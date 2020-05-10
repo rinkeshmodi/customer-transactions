ca2e7db0f789c6b6e705746d4be10034e01d8b03
rinkeshmodi <rinkeshmodi@gmail.com>
2020-05-10 11:39:48 +0530

    use read write lock for parallel reads

--- Customer-Transactions/src/main/java/my/test/banking/customer/transactions/repo/AccountsRepo.java
+++ Customer-Transactions/src/main/java/my/test/banking/customer/transactions/repo/AccountsRepo.java
@@ -9,47 +9,61 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Component
 public class AccountsRepo {
 
+
     @PostConstruct
     public void init(){
-        accountDTOMap = new ConcurrentHashMap<>();
+        accountDTOMap = new HashMap<>();
         AccountDTO accountDTO = new AccountDTO();
         accountDTO.setAccountNumber("100");
         accountDTO.setCustomerName("abc");
-        accountDTO.setAmount(new AtomicLong(500));
+        accountDTO.setAmount(new Long(500));
         accountDTOMap.put("100",accountDTO);
 
         AccountDTO accountDTO2 = new AccountDTO();
         accountDTO2.setAccountNumber("200");
         accountDTO2.setCustomerName("def");
-        accountDTO2.setAmount(new AtomicLong(1000));
+        accountDTO2.setAmount(new Long(1000));
         accountDTOMap.put("200",accountDTO2);
     }
 
-    private ConcurrentHashMap<String, AccountDTO> accountDTOMap;
+    private Map<String, AccountDTO> accountDTOMap;
 
+//    ReadWriteLock lock = new ReentrantReadWriteLock();
 
-    public AtomicLong getBalanceForAccountNumber(String accountNumber) {
+    public Long getBalanceForAccountNumber(String accountNumber) {
         if(accountDTOMap.containsKey(accountNumber)){
-            return accountDTOMap.get(accountNumber).getAmount();
+            AccountDTO accountDTO = accountDTOMap.get(accountNumber);
+            Long amount = null;
+            ReentrantReadWriteLock reentrantReadWriteLock = accountDTO.getReentrantReadWriteLock();
+            try{
+                reentrantReadWriteLock.readLock().lock();
+                amount = accountDTOMap.get(accountNumber).getAmount();
+            }finally {
+                reentrantReadWriteLock.readLock().unlock();
+            }
+            return amount;
         }
         throw new AccountServiceException("Account does not exist", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_ACCOUNT_NUMBER);
     }
 
-    public void addMoney(String accountNumber, String amount, int retryCount) {
-        if(retryCount == 5){
-            throw new AccountServiceException("Transaction declined", HttpStatus.INTERNAL_SERVER_ERROR, ErrorEnum.UNKNOWN_ERROR);
-        }
+    public void addMoney(String accountNumber, String amount) {
         if(accountDTOMap.containsKey(accountNumber)){
-            AtomicLong currentAmount = accountDTOMap.get(accountNumber).getAmount();
-            boolean addResult = currentAmount.compareAndSet(currentAmount.longValue(), Long.valueOf(amount).longValue() + currentAmount.longValue());
-            if(!addResult) {
-                addMoney(accountNumber, amount, retryCount + 1);
+            AccountDTO accountDTO = accountDTOMap.get(accountNumber);
+            ReentrantReadWriteLock reentrantReadWriteLock = accountDTO.getReentrantReadWriteLock();
+            try{
+                reentrantReadWriteLock.writeLock().lock();
+                Long currentAmount = accountDTO.getAmount();
+                accountDTO.setAmount(Long.valueOf(amount).longValue() + currentAmount.longValue());
+            }finally {
+                reentrantReadWriteLock.writeLock().unlock();
             }
         }else{
             throw new AccountServiceException("Account does not exist", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_ACCOUNT_NUMBER);
@@ -66,29 +80,32 @@ public class AccountsRepo {
             Collections.sort(accNumbers);
             AccountDTO firstAcc = accountDTOMap.get(accNumbers.get(0));
             AccountDTO secAcc = accountDTOMap.get(accNumbers.get(1));
-            synchronized (firstAcc){
-                synchronized (secAcc){
-                    AccountDTO fromAccount = accountDTOMap.get(fromAccountNumber);
-                    AccountDTO toAccount = accountDTOMap.get(toAccountNumber);
-                    //check if from account has enough balance
+            try{
+                firstAcc.getReentrantReadWriteLock().writeLock().lock();
+                secAcc.getReentrantReadWriteLock().writeLock().lock();
+                AccountDTO fromAccount = accountDTOMap.get(fromAccountNumber);
+                AccountDTO toAccount = accountDTOMap.get(toAccountNumber);
+                //check if from account has enough balance
 
-                    AtomicLong fromAccAmt = fromAccount.getAmount();
-                    System.out.println("Thread id : "+Thread.currentThread().getName());
-                    System.out.println("fromAccAmt" + fromAccAmt.longValue());
-                    AtomicLong amountToTransfer = new AtomicLong(Long.valueOf(amount));
-                    if(fromAccAmt.longValue() < amountToTransfer.longValue()) {
+                Long fromAccAmt = fromAccount.getAmount();
+                System.out.println("Thread id : "+Thread.currentThread().getName());
+                System.out.println("fromAccAmt" + fromAccAmt.longValue());
+                Long amountToTransfer = new Long(Long.valueOf(amount));
+                if(fromAccAmt.longValue() < amountToTransfer.longValue()) {
 
-                        throw new AccountServiceException("Insufficient Balance in account "+ fromAccount.getAccountNumber(), HttpStatus.BAD_REQUEST, ErrorEnum.INSUFFICIENT_BAL);
-                    }
-                    long finalFromAccAmount = fromAccAmt.addAndGet(- amountToTransfer.longValue());
-                    fromAccount.setAmount(new AtomicLong(finalFromAccAmount));
-                    accountDTOMap.put(fromAccountNumber, fromAccount);
-                    System.out.println("toAccount "+toAccount.getAmount().longValue());
-                    long finalToAccountAmt = toAccount.getAmount().addAndGet(amountToTransfer.longValue());
-                    toAccount.setAmount(new AtomicLong(finalToAccountAmt));
-                    accountDTOMap.put(toAccountNumber, toAccount);
+                    throw new AccountServiceException("Insufficient Balance in account "+ fromAccount.getAccountNumber(), HttpStatus.BAD_REQUEST, ErrorEnum.INSUFFICIENT_BAL);
                 }
+                long finalFromAccAmount = fromAccAmt - amountToTransfer.longValue();
+                fromAccount.setAmount(new Long(finalFromAccAmount));
+                accountDTOMap.put(fromAccountNumber, fromAccount);
+                System.out.println("toAccount "+toAccount.getAmount().longValue());
+                long finalToAccountAmt = toAccount.getAmount() + amountToTransfer.longValue();
+                toAccount.setAmount(new Long(finalToAccountAmt));
+                accountDTOMap.put(toAccountNumber, toAccount);
 
+            }finally {
+                secAcc.getReentrantReadWriteLock().writeLock().unlock();
+                firstAcc.getReentrantReadWriteLock().writeLock().unlock();
             }
         }else{
             throw new AccountServiceException("Account does not exist", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_ACCOUNT_NUMBER);
@@ -96,18 +113,19 @@ public class AccountsRepo {
 
     }
 
-    public void withDrawMoney(String accountNumber, String amount, int retryCount){
-        if(retryCount == 5){
-            throw new AccountServiceException("Transaction declined", HttpStatus.INTERNAL_SERVER_ERROR, ErrorEnum.UNKNOWN_ERROR);
-        }
+    public void withDrawMoney(String accountNumber, String amount){
+
         if(accountDTOMap.containsKey(accountNumber)){
-            AtomicLong currAmount = accountDTOMap.get(accountNumber).getAmount();
-            if(currAmount.longValue() <= 0){
-                throw new AccountServiceException("Can't withdraw ", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_AMOUNT);
-            }
-            boolean isWithdrawSuccess = currAmount.compareAndSet(currAmount.longValue(), currAmount.longValue() - Long.valueOf(amount));
-            if(!isWithdrawSuccess){
-                withDrawMoney(accountNumber, amount, retryCount + 1);
+            AccountDTO accountDTO = accountDTOMap.get(accountNumber);
+            try{
+                accountDTO.getReentrantReadWriteLock().writeLock().lock();
+                long currAmount = accountDTO.getAmount().longValue();
+                if(currAmount <= 0 || (currAmount - Long.valueOf(amount)) < 0){
+                    throw new AccountServiceException("Can't withdraw ", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_AMOUNT);
+                }
+                accountDTO.setAmount(currAmount - Long.valueOf(amount));
+            }finally {
+                accountDTO.getReentrantReadWriteLock().writeLock().unlock();
             }
         }else{
             throw new AccountServiceException("Account does not exist", HttpStatus.BAD_REQUEST, ErrorEnum.INVALID_ACCOUNT_NUMBER);
